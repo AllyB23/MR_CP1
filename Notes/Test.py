72c5beb52d9663cee93631c20c033b8d1b537f79
AllyB23 <marlin.rosales@ucas-edu.net>
2025-08-21 10:51:14 -0600

    variables notes

--- Notes/Test.py
+++ Notes/Test.py
@@ -1,3 +1,6 @@
+# MR 2nd Inputs and outputs notes
+
 print("test")
 #print() <= Everything inside the parenthesis appears in the terminal
-
+color = input("Give me a color: ")
+print(color)
---
+++ Practices/hello_world.py
@@ -0,0 +1,6 @@
+# MR 2nd Hello World
+print ("test")
+name = input("Give me your name: ")
+Hello= input("Hello ")
+print(name)
+
