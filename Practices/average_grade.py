41c138361a7711f64d9cae57873d064e231b46b6
AllyB23 <marlin.rosales@ucas-edu.net>
2025-08-29 10:33:21 -0600

    average grade

--- Practices/average_grade.py
+++ Practices/average_grade.py
@@ -3,5 +3,5 @@ CS = float(input( "What is your grade in computer science? "))
 world_civilizations = float(input( " What is your grade in world civilizations? "))
 advisory = float(input( "What is your grade in Advisory? "))
 Math = float(input( "Whats i your grade in Math? "))
-Biology =
-English =
+Biology = float(input("What is your grade in Biology? "))
+English = float(input( "What is your grade in English? "))
