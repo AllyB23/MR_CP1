65ae51d838a260c2c537133de0f5bae720209887
AllyB23 <marlin.rosales@ucas-edu.net>
2025-09-03 10:49:29 -0600

    Mad Libs

---
+++ Practices/madlib.py
@@ -0,0 +1,8 @@
+# MR 2nd madlib
+
+Adjective= input("Give me an adjective: ")
+Noun= input("Give me a Noun: ")
+Color= input("Give me a Color: ")
+Animal= input("Give me an Animal: ")
+Verb= input("Give me a Verb: ")
+print("It was a", Adjective "morning when I heard a knock on the", Noun " I opened it to find a"  )
