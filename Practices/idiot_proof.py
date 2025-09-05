63be87551f8431e8f3a97141e42c99aea7a3526b
AllyB23 <marlin.rosales@ucas-edu.net>
2025-09-05 10:49:23 -0600

    idiot proof

---
+++ Practices/idiot_proof.py
@@ -0,0 +1,5 @@
+# MR 2nd Idiot Proof
+
+first_name = input("What is your first and last name? ").capitalize().strip()
+phone_number = input("What is your phone number? ").capitalize().strip()
+GPA = input("What is your GPA? ")
