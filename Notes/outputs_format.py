48e2a584a8a551636de40610c273a68bd4f0118d
AllyB23 <marlin.rosales@ucas-edu.net>
2025-09-08 10:28:50 -0600

    format outputs notes

---
+++ Notes/outputs_format.py
@@ -0,0 +1,10 @@
+# MR 2nd format outputs notes
+
+name = "Jake"
+age = 21
+grade = .75
+money = 25
+
+print("Hello {}, mice to meet you!. You are {:b} that is really old! You have a {:%} in this class. You have ${:.2f}, that is a lot of money!".format(name, age, grade, money))
+
+print(f"Hello {name}, mice to meet you!. \nYou are {age:b} that is really old! \nYou have a {grade:%} in this class. \nYou have ${money:.2f}, that is a lot of money!")
