80f073ffb1c6e79b19966961ee7df998e2825178
AllyB23 <marlin.rosales@ucas-edu.net>
2025-09-15 10:51:02 -0600

    crew shares

--- Projects/crew_shares.py
+++ Projects/crew_shares.py
@@ -1,4 +1,10 @@
 # MR 2nd Crew Shares Project
 
-money =
-crew_members =
+money = float(input("What is the total amount of money?").strip())
+print("There is this much money: ", money)
+crew_members = int(input("How many crew members are there? " ).strip())
+shares = money/(crew_members+10)
+captain_share = shares*7
+first_mate = shares*3
+crew_share = shares-500
+print(f"The captain's share is ${captain_share:.2f} \n The first mate's share is ${first_mate:.2f}")
