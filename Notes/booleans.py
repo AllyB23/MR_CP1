ea2cdfb7172ec5107155c334dc5372122187c30a
AllyB23 <marlin.rosales@ucas-edu.net>
2025-09-18 10:51:04 -0600

    boolenas notes

---
+++ Notes/booleans.py
@@ -0,0 +1,34 @@
+# MR 2nd Booleans Notes
+
+import time
+import datetime as date
+
+over = False
+
+print(over)
+
+name = "Ms larose"
+
+print(bool(name))
+
+
+current_time = time.time()
+readable_time = time.ctime(current_time)
+
+print(f"Current Time in seconds since January 1, 1970(epoch time): {current_time}")
+print(f"Current Time: {readable_time}")
+
+now = date.datetime.now
+current_hour = now.hour()
+# Month = %m ( %b, %B )
+# day = %d
+# year = %Y, %y
+#hour = %H
+# mintues = %M
+#s seconds = %S
+
+print(f"Current time accroding to datetime: {now}")
+print(f"Hour: {}
+print(f"My hour variable is a String: {isinstance(hour, str)}")
+print(f"My hour variable is a Integer: {isinstance(hour, int)}")
+print(f"My hour variable is a Float: {isinstance(hour, float)}")
