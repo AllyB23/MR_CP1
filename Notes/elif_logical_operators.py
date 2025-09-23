246cc41fe570f1f35cd2e3b3211dd1cd7bfb458e
AllyB23 <marlin.rosales@ucas-edu.net>
2025-09-23 10:45:10 -0600

    Elif and Logical operators notes

---
+++ Notes/elif_logical_operators.py
@@ -0,0 +1,23 @@
+# MR 2nd Elif and Logical operators
+
+homework = True
+chores = True
+room = True
+
+if homework and chores:
+    print("You can go to your friends house.")
+elif not chores or not room:
+    print("Do your chores and clean your room!")
+else:
+    print("Go do your homework.")
+
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+
+if username == "MsLaRose" and password == "1234":
+    if password == "1234"
+        print("Welcome Ms. Larose")
+elif username == "Tia" and password == "password":
+    print("Welcome Tia")
+elif username == "Andrew" and password == "Orange":
+    print("That is not a valid username")
