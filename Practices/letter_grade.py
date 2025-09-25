5b79a639a55584461f4bd5a8f1a273ba0415024a
AllyB23 <marlin.rosales@ucas-edu.net>
2025-09-24 19:36:59 -0600

    Update letter_grade.py

--- Practices/letter_grade.py
+++ Practices/letter_grade.py
@@ -1,5 +1,14 @@
 # MR 2nd What is My grade
 
-grade = input("What is your grade percentage: ")
+grade = float(input("What is your grade percentage: "))
 
-if grade <60
+if grade >=90:
+  print(f"Your grade percentage is {grade}% and you have an A.")
+elif grade >= 80:
+  print(f"Your grade percentage is {grade}% and you have a B.")
+elif grade >=70:
+  print(f"Your grade percentage is {grade}% and you have a C.")
+elif grade >=60:
+  print(f"Your grade percentage is {grade}% and you have a D.")
+else:
+  print(f"Your grade percentage is {grade}% and you have an F.")
