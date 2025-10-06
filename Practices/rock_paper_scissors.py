df72b3cd5e135bbc70ef4cda4a283652c4a1a29c
AllyB23 <marlin.rosales@ucas-edu.net>
2025-10-06 10:50:29 -0600

    assignments

--- Practices/rock_paper_scissors.py
+++ Practices/rock_paper_scissors.py
@@ -1,3 +1,24 @@
 # MR 2nd Rock paper scissors
+import random
+import time
 
- 
+print("Welcome to Rock Paper Scissors")
+
+options = ["Rock", "Paper", "Scissors"]
+
+while True:
+    print("What would you like to do? ")
+    choice = input("Enter your choice(1-4): ")
+
+    if choice is "1":
+        print("You chose Rock!")
+
+        if choice is "2":
+            print("You chose Paper!")
+
+            if choice is "3":
+                print("You chose Scissors!")
+
+                if choice is "4":
+                    print("Thank you for playing!")
+
