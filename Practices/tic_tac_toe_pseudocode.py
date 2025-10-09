fb3d533ab649c7849c1f9c7086309ef4c0044960
AllyB23 <marlin.rosales@ucas-edu.net>
2025-10-09 10:46:45 -0600

    tic tac toe assignment

---
+++ Practices/tic_tac_toe_pseudocode.py
@@ -0,0 +1,21 @@
+# MR tic tac toe pseudocode
+
+# For loop a list ot make the tic tac toe board
+# def print_board(board)
+    #[1,2,3]
+    #[4,5,6]
+    #[7,8,9]
+    # print the board choices so the user can choose where to put their first mark
+# The player can only input an x so there has to be an if True statement to check what they are inputing
+    # If the input is invalid tell the user to "try again!"
+    # print the board and the choices on where to put the X.
+# use random.randint to choose the choice of the computer on where to put the O on the board
+
+# print both the players and the computers choices
+
+# If user choose to put the X in 1
+    #print the board with the 1 marked off 
+    # print the choice of the computer which is random 
+# Once a choice is taken on the board it can not be used again.
+# We need to make sure the the choice of the cumputer is different frrom th eusers choice
+# print 
