269f8be380daa02e98cb547cb695ffc8a63ac64a
AllyB23 <marlin.rosales@ucas-edu.net>
2025-10-21 10:05:06 -0600

    notes

---
+++ Notes/functions.py
@@ -0,0 +1,12 @@
+# MR 2nd functions notes
+
+num = 0
+def add(x,y):
+    return x+y
+
+while num < add(5,5):
+    print("Duck")
+    num+=1
+print("Goose")
+print(f"Results is: {add(-123,123456)}")
+total = add(9658765,78904567)
