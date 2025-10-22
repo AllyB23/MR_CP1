adabf2d79a98f97c3b27fc160ae2d2b98c308937
AllyB23 <marlin.rosales@ucas-edu.net>
2025-10-22 10:45:15 -0600

    caeser cipher

--- Practices/caesar_cipher.py
+++ Practices/caesar_cipher.py
@@ -1,8 +1,17 @@
 # MR 2nd Caeser Cipher encoder and decoder
 # first we will make a variable from a user input that they can choose to encode or decode
+print("Welcome to the Ceaser Cipher encoder and decoder!")
 action = input("Do you want to encode or decode a message: ")
+encode = input("What message do you want to encode:")
+decode = input("What is the message you want to decode: ")
+shift_value = input("How many times would you like to shift this message: ")
 
+def encode():
+    print(encode)
+    print("How many times would you like me to shift your message? ")
 
+def decode():
+    print(decode)
 
 # Then we will ask for the message that needs to be processed
 # Then we will ask for the shift value which has to be an integer
