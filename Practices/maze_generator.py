bfd9b58a185e0ed2f8b62290bb625007d801c5af
AllyB23 <marlin.rosales@ucas-edu.net>
2025-11-07 10:50:38 -0700

    turtles

--- Practices/maze_generator.py
+++ Practices/maze_generator.py
@@ -2,19 +2,79 @@
 # first we will import turtle and random
 import turtle
 import random
-#we have to set the variables for the grid columns and the grid rows
-grid_rows = [[1,2,3,],[4,5,6],[7,8,9]]
-grid_columns = 6
-start_point = (0,0)
-end_point = (5,5)
+#we have to set the variables for the grid columns and the grid rows, and all the other details of the grid
+
+num_cols = 5
+grid_rows = 5
+row_heights = 50
+grid_width = 300
+c_width  = 100
+start_point_x = -100
+starting_point_y = 100
+grid_rows = [
+    [0, 0, 0], 
+    [0, 1, 0], 
+    [0, 0, 0], 
+    [0, 0, 1], 
+    [0, 0, 0], 
+    [1, 0, 0]  
+]
+
+def draw_complete_grid(grid_rows, num_cols, rows_heights, grid_width, start_point_x, starting_point_y):
+    
+    t.penup()
+    t.goto(start_point_x, starting_point_y)
+    t.pendown
+
+    for i in range(grid_rows + 1): 
+
+        current_y = starting_point_y * row_heights
+        t.penup()
+        current_y = starting_point_y
+        t.goto(start_point_x, current_y)
+        t.pendown()
+        t.forward(num_cols * row_heights)
+
+    for i in range(num_cols + 1):
+        t.penup()
+        current_x = start_point_x + i * c_width
+        t.goto(current_x, starting_point_y)
+        t.pendown()
+        t.right(90)
+        t.forward(grid_rows * row_heights) 
+        t.left(90)
+def mark_cells(grid_rows, row_heights, c_width, start_poin_x, starting_point_y):
+    for row_index, row_data in enumerate(grid_rows):
+        for col_index, cell_value in enumerate(row_data):
+            if cell_value == 1:
+                # Calculate the center position of the cell
+                cell_center_x = start_point_x + col_index * c_width + c_width / 2
+                cell_center_y = starting_point_y - row_index * row_heights - row_heights / 2
+                t.penup()
+                t.goto(cell_center_x, cell_center_y)
+                t.pendown()
+draw_complete_grid(
+    grid_rows, 
+    num_cols, 
+    row_heights, 
+    c_width, 
+    start_point_x, 
+    starting_point_y
+)
+
+# Mark the cells using the data structure
+mark_cells(
+    grid_rows, 
+    row_heights, 
+    c_width, 
+    start_point_x, 
+    starting_point_y
+)
 
 
-turtle.pensize(10)
-turtle.fillcolor("black")
-turtle.begin_fill()
-for x in range (6):
-    turtle.forward(500)
-    turtle.end_fill()
+t = turtle.Turtle()
+t.speed(5)
+t.penup()
 # we need at least 3 functions
 # the first functions will be to randomly generate the grid columns with random open spaces in them
 
@@ -26,5 +86,5 @@ for x in range (6):
 # we will use turtle to draw the maze
 # we will code all the turtle setting so it looks like it is drawing the maze
 # turtle always has to move the same way but if it over runs then we have to check that\
-    turtle.penup()
-    turtle.done()
+turtle.penup()
+turtle.done()
