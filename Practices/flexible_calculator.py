79446a9f447f625948031486487943b2f4273c61
AllyB23 <marlin.rosales@ucas-edu.net>
2025-11-18 10:22:45 -0700

    changes

--- Practices/flexible_calculator.py
+++ Practices/flexible_calculator.py
@@ -3,6 +3,7 @@
 # write a print statement that welcome sthe user into the flexible calculator
 print("Welcome to the Flexible Calculator!")
 print("heyyyyy")
+
 # We will make args to show the avaiable operations we cand o for the calculator
 # we will mkae  afcuntions with an arg insode sp they can select which option they want
 # you want atnoeher function ot check if the number they input is an integer or a float
