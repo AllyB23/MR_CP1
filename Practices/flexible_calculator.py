835ca2232af61c789b32e730cb506819ebaa5971
AllyB23 <marlin.rosales@ucas-edu.net>
2025-11-18 10:49:39 -0700

    calculator

--- Practices/flexible_calculator.py
+++ Practices/flexible_calculator.py
@@ -2,10 +2,31 @@
 
 # write a print statement that welcome sthe user into the flexible calculator
 print("Welcome to the Flexible Calculator!")
-print("heyyyyy")
-
 # We will make args to show the avaiable operations we cand o for the calculator
+options = input("Which operation would you like to perform? avergae\n sum\n max\n min\n product\n")
+
+
 # we will mkae  afcuntions with an arg insode sp they can select which option they want
+def average(*nums):
+    int(input("Please enter the numbers that you want to average: "))
+
+
+def max(*nums):
+
+
+    def sum(*nums):
+        total = 0
+    
+
+
+        def min(*nums):
+
+
+                def product(*nums):
+                     r
+
+
+
 # you want atnoeher function ot check if the number they input is an integer or a float
 # you want to do an isinstance for calculating the numbers that they inputtedted and also the fucntions that they inputted
 # after all that you want to ive them the option to keeo going and do smome more calculations or the option ot be done por exit.
