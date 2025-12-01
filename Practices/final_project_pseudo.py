c0d81f225ad24ec0693341edcbada16b278ce810
AllyB23 <marlin.rosales@ucas-edu.net>
2025-12-01 10:37:05 -0700

    project

---
+++ Practices/final_project_pseudo.py
@@ -0,0 +1,32 @@
+# MR 2nd Final Project Pseudocode
+# We Will welcome the user into the game
+# Print("Welcome to Serverse Showdown!")
+# "Welcome to Serververse Showdown. The game has been hacked, the players are missing, and it's up to you to assemble the ultimate team and reclaim the court. Ready to play for a chance at freedom?"
+# We will explain the plot of the game using print statements
+# EXPLANATION
+# We will give the user a variable named Character so they can choose what character to be from the three options
+# The options will be LeBron James, Lola Bunny, and Daffy Duck.
+# We will make other variables that establish the stats of all the players.
+# No matter what character they choose they will have the same stats but they have different abilities
+# VARIABLES
+"Character:"
+"Health:"
+"Abilities:"
+"    "
+# FUNCTIONS/WORLDS
+"""Earth: LeBron James(player)
+DC World (Gotham city/ Metropolis): Daffy Duck(Player)
+Mad Max Universe: 
+Austin Powers World
+Wizard of Oz World
+Hogwarts(Harry Potter World)
+Casablanca world
+Serververse: Al-G Rythm(Villain/Antagonist)
+Thundera(Thundercats universe)
+Tune World: bugs bunny(character)"""
+
+# From each world they must recruit a player to move on to the next world
+# Make if statements to direct you to the rooms 
+# We will start with a function using nested loops or what if statements
+# 
+
