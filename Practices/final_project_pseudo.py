60b2953fd7f795d09095c14751ba62d93617ab93
AllyB23 <marlin.rosales@ucas-edu.net>
2025-12-02 20:32:37 -0700

    Update final_project_pseudo.py

--- Practices/final_project_pseudo.py
+++ Practices/final_project_pseudo.py
@@ -36,17 +36,24 @@ Casablanca world
 Serververse: Al-G Rythm(Villain/Antagonist)
 Thundera(Thundercats universe)
 Tune World: bugs bunny(character)"""
+# The user can basically choose what world or room is there starting one because there is not a set starting point
+# They will have the option to go to whatever world they like from the random 3 that they are given
 # User input to ask them what world they would like to go to
 # IF statements to decide which world to travel because they only have the the option of 3 to choose from
-
+# IF character = 1 go to Earth
+# Elif character = 2 go to 
 # DEF Earth():
+    # If the character is LeBron James then they start out on earth so we will make an if statement out of the function to lead the player here
+    # After that we will ask them to what world they want to to next using a user input
     # We will check earth if there is any players to recruit 
     # If there is none return False
 # DEF DC World():
+    # If the character is Daffy Duck then the player will come to this room/world and choose who to go recruit next
     # We will check earth if there is any players to recruit 
     # If there is none return False
 
 # DEF Mad Max Universe():
+    # if the character is Bugs Bunny the player wil start out on Mad Max Universe
     # We will check earth if there is any players to recruit 
     # If there is none return False
 # DEF Austin Powers World():
