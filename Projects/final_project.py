16d3b7f9bb883ce28c35cb1cafc345245fd9a0bc
AllyB23 <marlin.rosales@ucas-edu.net>
2025-12-08 10:43:12 -0700

    final

---
+++ Projects/final_project.py
@@ -0,0 +1,41 @@
+# MR 2nd Final Project
+import random
+import time
+
+choice = print(input("Welcome to Serververse Showdown.\n The game has been hacked, the players are missing,\n and it's up to you to assemble the ultimate team and reclaim the court.\n Ready to play for a chance at freedom?\n"))
+
+if choice == ("yes"):
+    print("okay! let's go!")
+elif choice ==("no"):
+    print("okay, thank you.")
+else:
+    print("Please enter a valid option.")
+#def print_slow(text):
+
+player_stats: {
+basketball_IQ: 5, 
+stamina = 5,
+looney_abilities = 5,
+strength = 5,
+speed = 5,
+shooting = 5}
+
+
+def earth():
+    pass
+def DC_World():
+    pass
+def Mad_Max_universe():
+    pass
+def Austin_powers_world():
+    pass
+def Wizard_of_oz_world():
+    pass
+def Hogwarts():
+    pass
+def casavlanca_world():
+    pass
+def Thundera():
+    pass
+def serververse():
+    pass
